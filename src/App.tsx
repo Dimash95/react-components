2ce67e4f445b9dd2dd4aebf41e9a55c38c7ca424
Dimash <dinmukhamed.amirov@gmail.com>
2023-10-29 16:03:50 +0600

    feat: add class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,87 @@
-import { useState, useEffect } from 'react';
+import { Component } from 'react';
 import { getAnime } from './api/get-anime';
 import Search from './components/search';
 import Card from './components/card';
 import { Item } from './entities/item';
 import { ItemResponse } from './entities/item-response';
 
-function App() {
-  const [items, setItems] = useState<Item[]>([]);
-  const [searchQuery, setSearchQuery] = useState('');
+interface AppState {
+  items: Item[];
+  searchQuery: string;
+}
 
-  async function displayItems(searchedAnime: string = '') {
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      items: [],
+      searchQuery: localStorage.getItem('Searched anime') || '',
+    };
+  }
+
+  // const [items, setItems] = useState<Item[]>([]);
+  // const [searchQuery, setSearchQuery] = useState('');
+
+  displayItems = async (searchedAnime: string = '') => {
     const itemResponses = (await getAnime(searchedAnime)) as unknown as {
       data: ItemResponse[];
     };
     if (itemResponses) {
       const fetchedItems = itemResponses.data.map(
-        (itemResponse: ItemResponse) => mapItemResponseToItem(itemResponse)
+        (itemResponse: ItemResponse) => this.mapItemResponseToItem(itemResponse)
       );
-      setItems(fetchedItems);
+      this.setState({ items: fetchedItems });
     }
-  }
+  };
 
-  const mapItemResponseToItem = (payload: ItemResponse): Item => ({
+  mapItemResponseToItem = (payload: ItemResponse): Item => ({
     title: payload.title,
     image: payload.images.webp.image_url,
     synopsis: payload.synopsis,
   });
 
-  useEffect(() => {
-    displayItems();
-  }, []);
+  // useEffect(() => {
+  //   displayItems();
+  // }, []);
 
-  function handleSearch() {
-    displayItems(searchQuery);
-    localStorage.setItem('Searched anime', searchQuery);
+  handleSearch = () => {
+    this.displayItems(this.state.searchQuery);
+    localStorage.setItem('Searched anime', this.state.searchQuery);
 
+    // const searchedAnime = localStorage.getItem('Searched anime');
+
+    // this.setState({ searchQuery: searchedAnime != null ? searchedAnime : '' });
+  };
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    this.setState({ searchQuery: value });
+  };
+
+  componentDidMount() {
     const searchedAnime = localStorage.getItem('Searched anime');
-    setSearchQuery(searchedAnime != null ? searchedAnime : '');
+    if (searchedAnime) {
+      this.setState({ searchQuery: searchedAnime }, () => {
+        this.displayItems(searchedAnime);
+      });
+    } else {
+      this.displayItems();
+    }
   }
 
-  return (
-    <div className="wrapper">
-      <Search
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-        handleSearch={handleSearch}
-      />
-      <Card items={items} />
-    </div>
-  );
+  render() {
+    return (
+      <div className="wrapper">
+        <Search
+          searchQuery={this.state.searchQuery}
+          setSearchQuery={(searchQuery) => this.setState({ searchQuery })}
+          handleSearch={this.handleSearch}
+        />
+        <Card items={this.state.items} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,26 +1,35 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Component } from 'react';
 import styles from './search.module.css';
 
 interface Props {
   searchQuery: string;
-  setSearchQuery: Dispatch<SetStateAction<string>>;
+  setSearchQuery: (value: string) => void;
   handleSearch: () => void;
 }
 
-function Search({ searchQuery, setSearchQuery, handleSearch }: Props) {
-  return (
-    <div className={styles.search}>
-      <input
-        className={styles.input}
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <button className={styles.button} onClick={handleSearch}>
-        Search
-      </button>
-    </div>
-  );
+class Search extends Component<Props> {
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    this.props.setSearchQuery(value);
+  };
+
+  render() {
+    const { searchQuery, handleSearch } = this.props;
+
+    return (
+      <div className={styles.search}>
+        <input
+          className={styles.input}
+          type="text"
+          value={searchQuery}
+          onChange={this.handleChange}
+        />
+        <button className={styles.button} onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Search;
