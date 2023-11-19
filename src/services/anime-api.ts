d5074751865cb4455721108e4a5ebdc007bf8969
Dimash <dinmukhamed.amirov@gmail.com>
2023-11-20 01:50:52 +0600

    fix: change tests

--- src/services/anime-api.ts
+++ src/services/anime-api.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const animeApi = createApi({
   reducerPath: 'animeApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.jikan.moe/v4/anime' }),
+
   endpoints: (builder) => ({
     getAnime: builder.query({
       query: ({ pageNumber = 1, itemsPerPage = 10, searchValue = '' }) =>
