648227e28fe8c6e37870cd3be42926a0bdb00cd0
Dimash <dinmukhamed.amirov@gmail.com>
2023-11-20 01:55:33 +0600

    fix: change tests

--- src/services/anime-api.ts
+++ src/services/anime-api.ts
@@ -1,8 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import fetch from 'isomorphic-fetch'; // Импорт функции fetch, совместимой с SSR
 
 export const animeApi = createApi({
   reducerPath: 'animeApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.jikan.moe/v4/anime' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.jikan.moe/v4/anime',
+    fetchFn: fetch,
+  }),
 
   endpoints: (builder) => ({
     getAnime: builder.query({
