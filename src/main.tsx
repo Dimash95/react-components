e3b85ad28cba0c32606dca4ed45f13474da68d5d
Dimash <dinmukhamed.amirov@gmail.com>
2023-11-19 09:54:34 +0600

    feat: add redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import ErrorBoundary from './components/error-boundary';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ErrorBoundary>
   </React.StrictMode>
 );
